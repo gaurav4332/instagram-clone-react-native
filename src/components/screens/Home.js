1594aca35061c924127ae0b44bb0f482d66daa89
gaurav4332 <gaurav4332@gmail.com>
2024-02-23 08:39:28 +0530

    Instagram clone Completed

---
+++ src/components/screens/Home.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {View, Text, StatusBar, ScrollView} from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionic from 'react-native-vector-icons/Ionicons';
+import Stories from '../screenComponents/Stories';
+import Post from '../screenComponents/Post';
+import { SafeAreaView } from 'react-native';
+
+const Home = () => {
+  return (
+    <View style={{backgroundColor: 'white', height: '100%'}}>
+      {/* <StatusBar
+        backgroundColor="white"
+        barStyle="dark-content"
+        animated={true}
+      /> */}
+      
+      <SafeAreaView>
+      <View
+        style={{
+          justifyContent: 'space-between',
+          flexDirection: 'row',
+          paddingHorizontal: 15,
+          alignItems: 'center',
+        }}>
+        <FontAwesome name="plus-square-o" style={{fontSize: 24}} />
+        <Text
+          style={{
+            fontFamily: 'Lobster-Regular',
+            fontSize: 25,
+            fontWeight: '500',
+          }}>
+          Instagram
+        </Text>
+        <Feather name="navigation" style={{fontSize: 24}} />
+      </View>
+
+      <ScrollView>
+        <Stories />
+        <Post />
+        <View
+          style={{justifyContent: 'center', alignItems: 'center', padding: 20}}>
+          <Ionic
+            name="ios-reload-circle-sharp"
+            style={{fontSize: 60, opacity: 0.2}}
+          />
+        </View>
+      </ScrollView>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default Home;
